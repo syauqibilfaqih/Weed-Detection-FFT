cfe67106ebce84580c98ad401db749ef1097ea1e
Ali Syauqi Bilfaqih <70939903+syauqibilfaqih@users.noreply.github.com>
2021-09-13 16:58:24 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+#Import libraries
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Load Image
+image = cv2.imread("images/Maize1000x714-1000x714.png")
+cv2.imshow("Image", image)
+
+#Green Color Detection
+#Color models in HSV
+blur = cv2.blur(image,(5,5))
+blur0=cv2.medianBlur(blur,5)
+blur1= cv2.GaussianBlur(blur0,(5,5),0)
+blur2= cv2.bilateralFilter(blur1,9,75,75)
+cv2.imshow("Blured",blur2)
+hsv = cv2.cvtColor(blur2, cv2.COLOR_BGR2HSV)
+dark_green = np.array([45 , 100, 50] )
+light_green = np.array([50, 255, 255])
+mask = cv2.inRange(hsv, dark_green, light_green)
+cv2.imshow("Masked",mask)
+result= cv2.bitwise_and(image,image, mask= mask)
+cv2.imshow("Result",result)
+#light_green = np.array([120,100,58.8])
+#dark_green = np.array([120, 100, 78.5])
+#image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+#mask = cv2.inRange(hsv_image, light_green, dark_green)
+#result = cv2.bitwise_and(image, image, mask=mask)
+#cv2.imshow("Result", result)
+
+
+#Gray-Scaling
+
+
+#Edge Detection
+
